6e55eab770b515fe367449cffabe2076f726fdbe
Aya Idrissi <aya.zoujari.id@gmail.com>
2025-01-28 12:58:01 +0100

    admin_CheckRole

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -8,12 +8,30 @@ use Illuminate\Support\Facades\Auth;
 
 class CheckRole
 {
-    public function handle(Request $request, Closure $next, $role)
+     public function handle(Request $request, Closure $next, $role)
     {
-        if (!Auth::check() || Auth::user()->role !== $role) {
+       /* if (!Auth::check() || Auth::user()->role !== $role) {
             abort(403, 'Unauthorized');
         }
 
-        return $next($request);
-    }
+        return $next($request);*/
+
+        if (!Auth::check()) {
+            return redirect('login');
+        }
+    
+        if (empty($roles)) {
+            return $next($request);
+        }
+    
+        foreach ($roles as $role) {
+            if (Auth::user()->role === $role) {
+                return $next($request);
+            }
+        }
+    
+        abort(403, 'Unauthorized action.');
+    } 
+
+    
 }
